d426194a5ac3d31837379333968940249df4dcb0
kidow <wcgo2ling@hanmail.net>
2019-07-02 00:56:01 +0900

    useDispatch, useSelector

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 import PropTypes from 'prop-types'
 import PostForm from '../components/PostForm'
 import PostCard from '../components/PostCard'
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { loginAction } from '../reducers/user'
 
 const dummy = {
   isLoggedIn: true,
@@ -18,6 +21,12 @@ const dummy = {
 }
 
 const Index = () => {
+  const dispatch = useDispatch()
+  const user = useSelector(state => state.user)
+  console.log('user', user)
+  useEffect(_ => {
+    dispatch(loginAction)
+  }, [])
   return (
     <div>
       {dummy.isLoggedIn && <PostForm />}
